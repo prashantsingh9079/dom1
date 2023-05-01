bfd2382b821e514f74cbffe5b38d5489928a3922
prashantsingh9079 <prashant.singh9079@gmail.com>
2023-05-01 13:58:31 +0530

    tagname commit

--- dom.js
+++ dom.js
@@ -1,21 +1,34 @@
-document.title='title changed from dom'
-console.log(document.title)
-var ht = document.getElementById("header-title")
-ht.textContent="changed from dom"
-//ht.innerText=<h1>rahuk </h1>
-var htt = document.getElementById("main-header")
-htt.style.borderBottom = 'solid 3px #000'
-var xy = document.getElementById("add-item")
-xy.style.fontWeight='Bold'
-xy.style.color='green'
+// document.title='title changed from dom'
+// console.log(document.title)
+// var ht = document.getElementById("header-title")
+// ht.textContent="changed from dom"
+// //ht.innerText=<h1>rahuk </h1>
+// var htt = document.getElementById("main-header")
+// htt.style.borderBottom = 'solid 3px #000'
+// var xy = document.getElementById("add-item")
+// xy.style.fontWeight='Bold'
+// xy.style.color='green'
 
 
 
-var item = document.getElementsByClassName("list-group-item")
-console.log(item)
-item[2].style.backgroundColor='green'
+// var item = document.getElementsByClassName("list-group-item")
+// console.log(item)
+// item[2].style.backgroundColor='green'
 
-for(let i=0;i<item.length;i++)
+// for(let i=0;i<item.length;i++)
+// {
+//     item[i].style.fontWeight='bold'
+// }
+
+var licn = document.getElementsByClassName("list-group-item")
+for(let k=0; k<licn.length; k++)
 {
-    item[i].style.fontWeight='bold'
+    licn[k].style.backgroundColor='lightblue'
+}
+
+var tn = document.getElementsByTagName('li')
+console.log(tn)
+for(let k=0; k<tn.length; k++)
+{
+    tn[k].style.fontWeight='bold'
 }
