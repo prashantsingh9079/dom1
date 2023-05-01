40a7d894be89c9d4d239743d689571e6dc5fef31
prashantsingh9079 <prashant.singh9079@gmail.com>
2023-05-01 12:48:55 +0530

    added getelemnetbyclassname

--- dom.js
+++ dom.js
@@ -2,9 +2,20 @@ document.title='title changed from dom'
 console.log(document.title)
 var ht = document.getElementById("header-title")
 ht.textContent="changed from dom"
-
+//ht.innerText=<h1>rahuk </h1>
 var htt = document.getElementById("main-header")
 htt.style.borderBottom = 'solid 3px #000'
 var xy = document.getElementById("add-item")
 xy.style.fontWeight='Bold'
 xy.style.color='green'
+
+
+
+var item = document.getElementsByClassName("list-group-item")
+console.log(item)
+item[2].style.backgroundColor='green'
+
+for(let i=0;i<item.length;i++)
+{
+    item[i].style.fontWeight='bold'
+}
