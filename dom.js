0a66c2857b71c813e5f3161ae111fa5649c78f66
prashantsingh9079 <prashant.singh9079@gmail.com>
2023-05-01 18:47:56 +0530

    dom2

--- dom.js
+++ dom.js
@@ -33,12 +33,52 @@
 //     tn[k].style.fontWeight='bold'
 // }
 
-var i2 = document.querySelectorAll('.list-group-item');
-i2[1].style.backgroundColor='green'
-i2[2].innerText=''
+// var i2 = document.querySelectorAll('.list-group-item');
+// i2[1].style.backgroundColor='green'
+// i2[2].innerText=''
 
-var o = document.querySelectorAll('li:nth-child(odd)');
-for(let k=0; k<o.length;k++ )
-{
-    o[k].style.backgroundColor='#90EE90';
-}
+// var o = document.querySelectorAll('li:nth-child(odd)');
+// for(let k=0; k<o.length;k++ )
+// {
+//     o[k].style.backgroundColor='#90EE90';
+// }
+
+var x = document.querySelector('#items')
+// console.log(x.parentElement)
+x.parentElement.style.backgroundColor='pink'
+
+// var y = document.querySelector('#items')
+// var z=y.children
+// z[1].style.backgroundColor='#ccc'
+var y = document.querySelector('#items')
+//console.log(y.lastElementChild)
+y.lastElementChild.textContent='lucky'
+console.log(y.lastChild)
+
+console.log(y.firstChild)
+y.firstElementChild.textContent='ITEM-1(ONE)'
+console.log(y.nextSibling)
+var l = document.querySelector('#add-item')
+console.log(l.nextElementSibling)
+l.nextElementSibling.style.backgroundColor='#ccc'
+console.log(y.previousSibling)
+y.previousElementSibling.style.color='red'
+
+var elem = document.createElement('h1');
+
+elem.className='hello';
+elem.id='hello1'
+
+//elem.setAttribute('title','hello div')
+var newDiv = document.createTextNode("HEllo")
+elem.appendChild(newDiv)
+console.log(elem)
+
+var cont = document.querySelector('#main')
+console.log(cont)
+var h2 = document.querySelector('#main #abc')
+cont.insertBefore(elem,h2)
+
+var cont2 = document.querySelector('.container')
+var h1 = document.querySelector('.container #header-title')
+cont2.insertBefore(elem,h1)
